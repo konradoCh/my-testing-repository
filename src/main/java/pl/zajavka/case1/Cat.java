e9b5a78c42c29b423a1dfad8a397f041cd9ced86
Testowy Koleszka <testowykoleszka@zajavka.pl>
2023-10-02 11:50:04 +0200

    Modification 1
    (cherry picked from commit 74f70eaaacfa5487af0c908afdc41b79c3a493f6)

--- src/main/java/pl/zajavka/case1/Cat.java
+++ src/main/java/pl/zajavka/case1/Cat.java
@@ -2,12 +2,6 @@ package pl.zajavka.case1;
 
 public class Cat extends Animal {
 
-    private String milk;
-
-    public Cat(String milk) {
-        this.milk = milk;
-    }
-
     @Override
     public String whatIsYourName() {
         return "My name is Cat";
